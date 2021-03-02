ab066ef5f3ae368dcd72a40e75748c181e17e143
tunv <nguyenvantu191298@gmail.com>
2021-03-02 22:08:39 +0700

    complete

--- src/views/Categories/index.js
+++ src/views/Categories/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
+import categories from '../../services/categories';
+import ListCategories from '../../components/List/ListCategories';
 
 function Categories(props) {
+  const [cate, setCate] = useState();
+
+  useEffect(() => {
+    const fetchC = async () => {
+      const response = await categories.getAll();
+
+      if (response && response.data.is_success) {
+        console.log('C', response.data.data.data);
+        setCate(response.data.data.data);
+      }
+    };
+
+    fetchC();
+    return () => {};
+  }, []);
+
   return (
     <>
       <Helmet>
         <title>Danh Mục</title>
       </Helmet>
-      <div>Categories</div>
+      <ListCategories title='Danh mục sản phẩm' list={cate} />
     </>
   );
 }
--- src/views/Products/index.js
+++ src/views/Products/index.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
+import products from '../../services/products';
+import List from '../../components/List';
 
 function Products(props) {
+  const [pro, setPro] = useState([]);
+
+  useEffect(() => {
+    const fetchP = async () => {
+      const response = await products.getAll();
+
+      if (response && response.data.is_success) {
+        console.log('Products', response.data);
+        setPro(response.data.data.data);
+      }
+    };
+
+    fetchP();
+    return () => {};
+  }, []);
+
   return (
     <>
       <Helmet>
         <title>Sản Phẩm</title>
       </Helmet>
-      <div>Products</div>
+
+      <div className=''>
+        <List list={pro} title='Tất cả sản phẩm' />
+      </div>
     </>
   );
 }
